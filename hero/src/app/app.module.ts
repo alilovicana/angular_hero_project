2085aa7b332b5824e387a96903de2246290ccccf
alilovicana <97240452+alilovicana@users.noreply.github.com>
2023-07-31 16:00:29 +0200

    implement reset()

--- hero/src/app/app.module.ts
+++ hero/src/app/app.module.ts
@@ -3,26 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { DialogComponent } from './dialog/dialog.component';
-import {MatDialogModule} from '@angular/material/dialog';
-import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatSelectModule} from '@angular/material/select';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DialogComponent
-  ],
+  declarations: [AppComponent, DialogComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -37,9 +37,11 @@ import {MatNativeDateModule} from '@angular/material/core';
     MatRadioModule,
     MatSelectModule,
     MatNativeDateModule,
-    MatDatepickerModule
+    MatDatepickerModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- hero/src/app/dialog/dialog.component.ts
+++ hero/src/app/dialog/dialog.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-dialog',
@@ -6,21 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dialog.component.css'],
 })
 export class DialogComponent implements OnInit {
+  myForm!: FormGroup;
   age = ['New', 'Second Hand', 'Refabrished'];
   type = ['Fruit', 'Car', 'Animal'];
-  constructor() {}
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) {}
   onSubmit() {
-    console.log(this.dialog);
+    const url = 'http://localhost:3000/products';
+    const formData = this.myForm.value;
+    this.http.post(url, formData).subscribe(
+      (response) => {
+        console.log('Podaci su spremljeni u db.json:', response);
+      },
+      (error) => {
+        console.error('Greška pri spremanju podataka:', error);
+      }
+    );
+    this.myForm.reset();
   }
+
   dialog: {
     name: string;
     price: string;
     type: string;
-    selectedDate: Date| null;
+    selectedDate: Date | null;
     selectedAge: string;
   } = { name: '', price: '', type: '', selectedDate: null, selectedAge: '' };
 
   ngOnInit(): void {
-    console.log(this.dialog.name);
+    this.myForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      price: ['', Validators.required],
+      type: [''],
+      selectedDate: [''],
+      selectedAge: [''],
+    });
   }
 }
