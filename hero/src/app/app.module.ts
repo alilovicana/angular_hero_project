626bfc0b6027c7eb8e4e248d3a12423c616186e1
alilovicana <97240452+alilovicana@users.noreply.github.com>
2023-07-31 10:04:05 +0200

    objekat u koji spremam dijelove forme

--- hero/src/app/app.module.ts
+++ hero/src/app/app.module.ts
@@ -18,8 +18,6 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatNativeDateModule} from '@angular/material/core';
 
-
-
 @NgModule({
   declarations: [
     AppComponent,
--- hero/src/app/dialog/dialog.component.ts
+++ hero/src/app/dialog/dialog.component.ts
@@ -9,6 +9,18 @@ export class DialogComponent implements OnInit {
   age = ['New', 'Second Hand', 'Refabrished'];
   type = ['Fruit', 'Car', 'Animal'];
   constructor() {}
+  onSubmit() {
+    console.log(this.dialog);
+  }
+  dialog: {
+    name: string;
+    price: string;
+    type: string;
+    selectedDate: Date| null;
+    selectedAge: string;
+  } = { name: '', price: '', type: '', selectedDate: null, selectedAge: '' };
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.dialog.name);
+  }
 }
